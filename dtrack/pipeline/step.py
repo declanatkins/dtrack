1248b93bedeca6a6ec5345e07e9ff852e5f27409
Declan Atkins <declanatkins@gmail.com>
2023-01-11 15:45:04 +0000

    Added pipeline step decorator

--- dtrack/pipeline/step.py
+++ dtrack/pipeline/step.py
@@ -4,7 +4,9 @@ from .arguments import PipelineArgument
 
 
 class PipelineStep:
-    """A callable object that can be used in a pipeline.
+    """A callable object that can be used in a pipeline. Instead of creating this class
+        directly, use the pipeline_step decorator.
+
     """
 
     def __init__(self, name: str, function: Callable, *args: Iterable[PipelineArgument]):
@@ -15,3 +17,11 @@ class PipelineStep:
     def __call__(self, context: ApplicationContext):
         args = [arg.evaluate(context) for arg in self.args]
         return self.function(*args)
+
+
+def pipeline_step(name: str, *args: Iterable[PipelineArgument]):
+    """A decorator that can be used to create a pipeline step.
+    """
+    def wrapper(function):
+        return PipelineStep(name, function, *args)
+    return wrapper
